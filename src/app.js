372cf455224048321ae16385eb77f851baa05f03
minuantony <minuantony19@gmail.com>
2021-07-16 23:55:36 -0500

    Showing the current temperature.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function Weather(response) {
+  console.log(response);
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data.name);
+  let placeElement = document.querySelector("#place");
+  placeElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity + "%";
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
+let apiKey = "04dd91307ec56c5bdc7c8b53d6799c73";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
 
+axios.get(apiUrl).then(Weather);
