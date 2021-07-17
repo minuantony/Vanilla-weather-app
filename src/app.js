8bcf0313b8d26a49d8ed37b81d9203cd397b5136
minuantony <minuantony19@gmail.com>
2021-07-17 10:35:30 -0500

    Added relevant weather icon from API

--- src/app.js
+++ src/app.js
@@ -23,24 +23,30 @@ function formatTime(timestamp) {
 }
 
 function Weather(response) {
-  console.log(response);
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  console.log(response.data.name);
   let placeElement = document.querySelector("#place");
   placeElement.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].main;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity + "%";
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = response.data.wind.speed;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatTime(response.data.dt);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
 }
 let apiKey = "04dd91307ec56c5bdc7c8b53d6799c73";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(Weather);
